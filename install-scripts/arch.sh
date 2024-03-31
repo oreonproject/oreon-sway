ea59c5de931c04c1a701bc7e06b40e244f8a8e4d
Hellx2 <ethan.oleary@contactspace.com>
2024-04-01 09:27:23 +1100

    Put usr/ and etc/ into the src/ folder and add an install script for Arch Linux.

---
+++ install-scripts/arch.sh
@@ -0,0 +1,23 @@
+sudo pacman -S --needed swappy xdg-utils sway\
+    swaybg waybar xorg-xwayland wf-recorder\
+    mako polkit xdg-desktop-portal-gtk\
+    xdg-desktop-portal-wlr otf-font-awesome\
+    wofi base-devel
+
+if ! command -v paru; then
+    mkdir -p ~/.cache/paru/clone/
+    git clone https://github.com/Morganamilo/paru ~/.cache/paru/clone/paru
+    cd ~/.cache/paru/clone/paru
+    makepkg -si
+fi
+
+paru -S sway-interactive-screenshot dragon-drop
+
+mkdir -p ~/.cache/oreon
+cd ~/.cache/oreon
+git clone --depth=1 https://github.com/oreonproject/oreon-sway.git ~/.cache/oreon/oreon-sway
+
+cd ~/cache/oreon/oreon-sway/src
+sudo cp -R usr/share/* /usr/share
+sudo cp -R etc/sway /etc/sway
+sudo cp -R etc/xdg/* /etc/xdg
